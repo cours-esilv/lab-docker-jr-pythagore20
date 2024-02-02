429786ad30708fd54d8afdc2a73e116dfdafb474
jr-pythagore20 <128241586+jr-pythagore20@users.noreply.github.com>
2024-02-02 10:30:55 +0100

    Update app.py

--- app/back/app.py
+++ app/back/app.py
@@ -6,16 +6,12 @@ from datetime import datetime
 
 if 'CURRENT_ENVIRONMENT' not in os.environ:
     print('===================================================================', file=sys.stderr)
-    print('[ERROR] Missing value for CURRENT_ENVIRONMENT environment variable.', file=sys.stderr)
+    print('[ERROR] Missing value for CURRENT_ENVIRONMENT envrionment variable.', file=sys.stderr)
     print('[ERROR] Please specify it when you start the container.', file=sys.stderr)
     sys.exit(1)
 
-# Check if the 'logs' directory exists before creating it
-logs_directory = './logs'
-if not os.path.exists(logs_directory):
-    os.mkdir(logs_directory)
-else:
-    print(f"The '{logs_directory}' directory already exists.")
+# Create logs folder
+os.mkdir('./logs')
 
 app = Flask(__name__)
 CORS(app)
@@ -27,7 +23,7 @@ def default():
         "environment": os.environ['CURRENT_ENVIRONMENT'],
         "hostname": os.uname()[1],
         "result": "root"
-    }
+    }    
 
 @app.route("/get/<name>")
 def get(name):
@@ -41,7 +37,7 @@ def get(name):
 @app.route("/write/<something>")
 def write(something):
     print(something)
-    with open(f'{logs_directory}/my-messages.log', 'a') as the_file:
+    with open('logs/my-messages.log', 'a') as the_file:
         the_file.write(f"{something}\n")
     return {
         "status": "ok"
